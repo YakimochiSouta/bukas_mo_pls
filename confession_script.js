a3684c819156f9a5a58639dd19462e1c256eb552
YakimochiSouta <160258538+YakimochiSouta@users.noreply.github.com>
2024-02-17 01:14:21 +0800

    final for confession

---
+++ confession_script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+
+const max_no = 9;
+
+let play = true;
+let noCount = 0;
+
+noButton.addEventListener("click", function () {
+    if (play) {
+        noCount++;
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === max_no) {
+      play = false;
+    }
+  }
+});
+
+
+function resizeYesButton() {
+    const computedStyle = window.getComputedStyle(yesButton);
+    const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+    const newFontSize = fontSize * 1.8;
+  
+    yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "Sure ka ba",
+    "Weh di nga",
+    "Yung totoo",
+    "Ayaw mo ba talaga",
+    "Iyak ako ahaha",
+    "Wala na sadboi na",
+    "Umiiyak na ako oh huhuhu",
+    "lombai",
+    "hahahaha",
+    "ayaw talaga sad :((",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
---
+++ confession_script2.js
@@ -0,0 +1,35 @@
+const app = document.getElementById('app');
+
+var Typewriter = new Typewriter(app, {
+    delay: 75,
+});
+
+Typewriter
+    .pauseFor(2000)
+    .typeString('Dear Jossel,')
+    .pauseFor(2000)
+    .typeString('<br> <br> Sorry if I made a crappy code just to confess something to you HAHA T^T .')
+    .pauseFor(1000)
+    .typeString(" Di man ako marunong gumawa ng ganito pero I've done my best to confess with a style ay hala si koya.")
+    .pauseFor(1000)
+    .typeString(" Pero alam mo ba?")
+    .pauseFor(5000)
+    .typeString(" na I have a crush on you po wahahaha.")
+    .pauseFor(1000)
+    .typeString("<br> <br> AAAAAA sorry if I have to confess today T^T HAHAHAH")
+    .pauseFor(1000)
+    .typeString(" Simula pa toh nung first f2f natin sa chemistry this feeling dhbskzd ")
+    .pauseFor(1000)
+    .typeString(" I just need to take this off my chest and para malaman mo rin hehe ramdam na ramdam ko halatang halata na ako and I think hinihintay mo lang ako umamin sayo directly.")
+    .pauseFor(1000)
+    .typeString(" <br> <br> I just really want to let you know this feeling and it's up to you if want mo replyan.")
+    .pauseFor(1000)
+    .typeString("Thank you for making this 1st Sem much more interesting with you around as my classmate HAHAHAH ganado mag -aral seeing you shine with a smile.")
+    .pauseFor(1000)
+    .typeString(" <br> <br> Advance Happy Birthday din pala sayo Jossel!!! Continue to smile <3 your smile is one of a kind.")
+    .pauseFor(1000)
+    .typeString(" Hope in the future maabot mo ang iyong mga dreams. I am here rooting for u hehe")
+    .pauseFor(1000)
+    .typeString(" <br> <br> ~ Felmyr :p")
+    .pauseFor(1000)
+    .start()
